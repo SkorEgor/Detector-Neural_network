28ddcca045ece145a3e22ea9a1bf40ba977c57db
SkorEgor <e2622001@gmail.com>
2025-04-03 13:56:02 +0300

    Correct operation of variable settings

--- detector_neural_network/setting.py
+++ detector_neural_network/setting.py
@@ -4,10 +4,16 @@ from dotenv import load_dotenv
 load_dotenv()
 MODULE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-DEBUG_ALL: bool = os.getenv("DEBUG_ALL", False)
-USE_DEFAULT_FILE_PATH_WITHOUT_SUBSTANCE: bool = os.getenv("USE_DEFAULT_FILE_PATH_WITHOUT_SUBSTANCE", False)
-USE_DEFAULT_FILE_PATH_WITH_SUBSTANCE: bool = os.getenv("USE_DEFAULT_FILE_PATH_WITH_SUBSTANCE", False)
-USE_DEFAULT_FILE_PATH_NEURAL_NETWORK: bool = os.getenv("USE_DEFAULT_FILE_PATH_NEURAL_NETWORK", False)
+DEBUG_ALL: bool = os.getenv("DEBUG_ALL", "False").lower() == "true"
+USE_DEFAULT_FILE_PATH_WITHOUT_SUBSTANCE: bool = (
+    os.getenv("USE_DEFAULT_FILE_PATH_WITHOUT_SUBSTANCE", "False").lower() == "true"
+)
+USE_DEFAULT_FILE_PATH_WITH_SUBSTANCE: bool = (
+    os.getenv("USE_DEFAULT_FILE_PATH_WITH_SUBSTANCE", "False").lower() == "true"
+)
+USE_DEFAULT_FILE_PATH_NEURAL_NETWORK: bool = (
+    os.getenv("USE_DEFAULT_FILE_PATH_NEURAL_NETWORK", "False").lower() == "true"
+)
 DEFAULT_FILE_PATH_WITHOUT_SUBSTANCE: str = os.path.join(MODULE_DIR, "data", "example_spectrum", "without_substance.csv")
 DEFAULT_FILE_PATH_WITH_SUBSTANCE: str = os.path.join(MODULE_DIR, "data", "example_spectrum", "with_substance.csv")
 DEFAULT_FILE_PATH_NEURAL_NETWORK: str = os.path.join(MODULE_DIR, "data", "example_neural_network", "5_new.joblib")
